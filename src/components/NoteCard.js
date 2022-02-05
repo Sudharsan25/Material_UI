d7ff9db0e113d0f37903dbe36e9a6f604e9be04f
Sudharsan <sudharsan.0125@gmail.com>
2022-02-05 09:10:23 +0530

    NoteCard Component Done

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -5,13 +5,41 @@ import {
   CardContent,
   IconButton,
   Typography,
+  makeStyles,
+  Avatar,
 } from "@material-ui/core";
 import { DeleteOutlined } from "@material-ui/icons";
+import { blue, green, pink, yellow } from "@material-ui/core/colors";
+
+const useStyles = makeStyles(() => {
+  return {
+    avatar: {
+      backgroundColor: (note) => {
+        if (note.category == "Remainders") {
+          return yellow[700];
+        }
+        if (note.category == "Money") {
+          return green[500];
+        }
+        if (note.category == "Work") {
+          return pink[500];
+        }
+        return blue[500];
+      },
+    },
+  };
+});
 
 export default function NoteCard({ note, handleDelete }) {
+  const classes = useStyles(note);
   return (
     <Card elevation={2}>
       <CardHeader
+        avatar={
+          <Avatar className={classes.avatar}>
+            {note.category[0].toUpperCase()}
+          </Avatar>
+        }
         action={
           <IconButton onClick={() => handleDelete(note.id)}>
             <DeleteOutlined />

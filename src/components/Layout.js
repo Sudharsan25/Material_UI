65b3273aec6a9291b992cc47a1ef065e6bdf952e
Sudharsan <sudharsan.0125@gmail.com>
2022-02-05 09:10:43 +0530

    Added Avatars

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,29 +1,54 @@
 import React from "react";
 import { Drawer, makeStyles, Typography } from "@material-ui/core";
-import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  AppBar,
+  Toolbar,
+} from "@material-ui/core";
 import { AddCircleOutlined, SubjectOutlined } from "@material-ui/icons";
 import { useHistory } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { format } from "date-fns";
+import { Avatar } from "@material-ui/core";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles({
-  root: {
-    display: "flex",
-  },
-  page: {
-    background: "#f9f9f9",
-    width: "100%",
-  },
-  drawer: {
-    width: drawerWidth,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  active: {
-    backgroundColor: "#f4f4f4",
-  },
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      display: "flex",
+    },
+    page: {
+      background: "#f9f9f9",
+      width: "100%",
+      padding: theme.spacing(3),
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    active: {
+      backgroundColor: "#f4f4f4",
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+    appbar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+    toolbar: theme.mixins.toolbar,
+    date: {
+      flexGrow: 1,
+    },
+    avatar: {
+      marginLeft: theme.spacing(2),
+    },
+  };
 });
 
 export default function Layout(props) {
@@ -45,6 +70,16 @@ export default function Layout(props) {
   ];
   return (
     <div className={classes.root}>
+      <AppBar elevation="0" className={classes.appbar}>
+        <Toolbar>
+          <Typography className={classes.date} variant="h5">
+            Today is {format(new Date(), "do MMMM Y")}
+          </Typography>
+          <Typography variant="h5">User</Typography>
+          <Avatar src="/avatar-svgrepo-com.svg" className={classes.avatar} />
+        </Toolbar>
+      </AppBar>
+
       <Drawer
         className={classes.drawer}
         variant="permanent"
@@ -52,7 +87,9 @@ export default function Layout(props) {
         classes={{ paper: classes.drawerPaper }}
       >
         <div>
-          <Typography variant="h5">Ninja</Typography>
+          <Typography className={classes.title} variant="h5">
+            Ninja
+          </Typography>
           <List>
             {menuItem.map((item) => (
               <ListItem
@@ -70,7 +107,10 @@ export default function Layout(props) {
           </List>
         </div>
       </Drawer>
-      <div className={classes.page}>{props.children}</div>
+      <div className={classes.page}>
+        <div className={classes.toolbar}></div>
+        {props.children}
+      </div>
     </div>
   );
 }
